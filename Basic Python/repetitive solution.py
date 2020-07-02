1c51156391f8be7cfdd5064936303e09bdf52e19
khyunjiee <khyunjiee@gmail.com>
2020-07-03 02:50:53 +0900

    Study: 반복문을 이용한 문제풀이

---
+++ Basic Python/repetitive solution.py
@@ -0,0 +1,25 @@
+'''
+반복문을 이용한 문제풀이
+1) 1부터 N까지 홀수 출력하기
+2) 1부터 N까지의 합 구하기
+3) N의 약수 출력하기
+'''
+
+print("1) 1부터 N까지 홀수 출력하기")
+n = int(input())
+for i in range(1, n+1):
+    if i % 2 != 0:
+        print(i)
+
+print("2) 1부터 N까지의 합 구하기")
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    sum += i
+print(sum)
+
+print("3) N의 약수 출력하기")
+n = int(input())
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i, end = ' ')
