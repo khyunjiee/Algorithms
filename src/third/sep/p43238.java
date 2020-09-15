06a5708aafa2647d6f7c59bd89f86c6d72460ea5
khyunjiee <khyunjiee@gmail.com>
2020-09-15 15:19:54 +0900

    [스터디] 9월 3주차 알고리즘 스터디

---
+++ src/third/sep/p43238.java
@@ -0,0 +1,32 @@
+package third.sep;
+
+import java.util.Arrays;
+
+public class p43238 {
+    class Solution {
+        public long solution(int n, int[] times) {
+            Arrays.sort(times);
+            long min = 1;
+            long max = (long)times[times.length - 1] * n;
+            long mid = 0;
+            long sum;
+            long answer = max;
+
+            while (min <= max) {
+                sum = 0;
+                mid = (min + max) / 2;
+
+                for (int t : times) {
+                    sum += mid / t;
+                }
+
+                if (sum >= n) {
+                    if (mid < answer) answer = mid;
+                    max = mid - 1;
+                } else min = mid + 1;
+            }
+
+            return answer;
+        }
+    }
+}
