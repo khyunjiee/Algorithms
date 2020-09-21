0e8e9a106ede27f716195582b6470b33a6ad72d5
khyunjiee <khyunjiee@gmail.com>
2020-09-21 20:43:46 +0900

    [스터디] 9월 4주차 알고리즘

---
+++ src/forth/sep/p42746.java
@@ -0,0 +1,30 @@
+package forth.sep;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class p42746 {
+    class Solution {
+        public String solution(int[] numbers) {
+            String answer = "";
+            String[] arr = new String[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++) {
+                arr[i] = (String.valueOf(numbers[i]));
+            }
+            Arrays.sort(arr, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return (o2 + o1).compareTo(o1 + o2);
+                }
+            });
+
+            if (arr[0].equals("0")) return "0";
+            for (String str : arr) {
+                answer += str;
+            }
+
+            return answer;
+        }
+    }
+}
