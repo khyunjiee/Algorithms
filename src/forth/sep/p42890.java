0caa4616b19219747611b29e3521a6164e14eacb
khyunjiee <khyunjiee@gmail.com>
2020-09-27 17:21:30 +0900

    [스터디] 9월 4주차 알고리즘 스터디 완료

---
+++ src/forth/sep/p42890.java
@@ -0,0 +1,41 @@
+package forth.sep;
+
+import java.util.HashSet;
+
+public class p42890 {
+    class Solution {
+        public int solution(String[][] relation) {
+            int answer = 0;
+            int column = relation[0].length;
+            int row = relation.length;
+            HashSet<String> selects = new HashSet<>();
+            HashSet<Integer> candidates = new HashSet<>();
+
+            for (int i = 1; i < (1 << column); i++) {
+                selects.clear();
+
+                for (int j = 0; j < row; j++) {
+                    String data = "";
+                    for (int k = 0; k < column; k++) {
+                        if ((i & (1 << k)) != 0) {
+                            data += relation[j][k] + ",";
+                        }
+                    }
+                    selects.add(data);
+                }
+                if (selects.size() == row) {
+                    push(candidates, i);
+                }
+            }
+
+            return candidates.size();
+        }
+
+        private void push(HashSet<Integer> candidates, int set) {
+            for (int key : candidates) {
+                if ((key & set) == key) return;
+            }
+            candidates.add(set);
+        }
+    }
+}
