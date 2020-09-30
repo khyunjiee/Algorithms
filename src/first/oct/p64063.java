13c83011fefff2816bead9c68af228563711e1ed
khyunjiee <khyunjiee@gmail.com>
2020-09-30 21:45:06 +0900

    [스터디] 10월 1주차 알고리즘 스터디

---
+++ src/first/oct/p64063.java
@@ -0,0 +1,31 @@
+package first.oct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class p64063 {
+    class Solution {
+        Map<Long, Long> hotel = new HashMap<>();
+
+        public long[] solution(long k, long[] room_number) {
+            long[] answer = new long[room_number.length];
+
+            for (int i = 0; i < room_number.length; i++) {
+                answer[i] = emptyRoom(room_number[i]);
+            }
+
+            return answer;
+        }
+
+        private long emptyRoom(long room) {
+            if (!hotel.containsKey(room)) {
+                hotel.put(room, room + 1);
+                return room;
+            }
+            long nextRoom = hotel.get(room);
+            long emptyRoom = emptyRoom(nextRoom);
+            hotel.put(room, emptyRoom);
+            return emptyRoom;
+        }
+    }
+}
