f6b6d2456f3a7303ae1de4aea6f7d2d2a90d3c8f
khyunjiee <khyunjiee@gmail.com>
2020-10-17 21:14:08 +0900

    [스터디] 10월 3주차 알고리즘 스터디

---
+++ src/oct/third/p68646.java
@@ -0,0 +1,24 @@
+package oct.third;
+
+public class p68646 {
+    class Solution {
+        public int solution(int[] a) {
+            int answer = 0;
+            int l = 1000000000, r = 1000000000;
+
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] < l) {
+                    answer++;
+                    l = a[i];
+                }
+                if (a[a.length - 1 - i] < r) {
+                    answer++;
+                    r = a[a.length - 1 - i];
+                }
+                if (l == r)
+                    break;
+            }
+            return answer + (l == r ? -1 : 0);
+        }
+    }
+}
